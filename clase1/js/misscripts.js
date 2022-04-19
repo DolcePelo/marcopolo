a689e0146e52bd907570ea38ffe23755b05e9bf2
andy <arteysoft@gmail.com>
2022-04-18 21:06:44 -0300

    ejercicio 1

--- clase1/js/misscripts.js
+++ clase1/js/misscripts.js
@@ -1,4 +1,48 @@
+// scope de las variables ()
 
+/*
+console.log('hola')
 
+;(function funcion1() {
+    {
+        var var1 = 'Variable 1'
+    }
+    {
+        console.log(var1)
+    }
+})()
+*/
 
+// Paradigma funcional
+// functions as data
 
+// function decirAlgo(fn) {
+//     fn('Algo')
+// }
+
+// decirAlgo(function(z) {
+//     alert(z + ' 1!')
+//     alert(z + ' 2!')
+//     alert(z + ' 3!')
+// })
+
+// Guarda con el single thread
+// let miDisplay = document.getElementById('miDisplay');
+// for (let x=1; x <= 1000000; x++) {
+//     miDisplay.innerHTML = x
+// }
+
+// operador condicional
+// let saldo = 100
+// let disponible = (saldo >= 10000) ? 'tiene disp' : 'no tiene disp'
+// alert(disponible)
+
+// ejercicio.
+// Tengo una funcion
+
+function operar(a, b, fnOperador) {
+
+}
+
+// tengo que llamar a operar con los valores de a y b y ademas
+// pasarle la operacion que quiero realizar
