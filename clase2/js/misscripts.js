2771f9521391389a572399f13e0ad367954812dc
andy <arteysoft@gmail.com>
2022-04-20 20:56:04 -0300

    ejercicio 1

---
+++ clase2/js/misscripts.js
@@ -0,0 +1,49 @@
+// Tipos de datos
+
+/*
+function recibeUnTipoDeDato(z) {
+    //z. = 'Hola amigos'
+}
+
+{
+    let var1 = {
+        nombre: 'Santiago',
+        apellido: 'Rodriguez',
+        campeonatos: [2015, 1018, 2021]
+    }
+    var1 = [1,2,3,4,5]
+    var1 = 'hola'
+
+    recibeUnTipoDeDato(_.clone(var1))
+    document.getElementById('miDisplay').innerHTML =
+                                    JSON.stringify(var1)
+}
+*/
+
+/* Primera aproximacion al manejo de eventos
+{
+    let miDisplay = document.getElementById('miDisplay')
+    function cambiarTamanio() {
+        miDisplay.innerHTML = window.innerWidth
+    }
+    window.addEventListener('resize', cambiarTamanio)
+}
+*/
+
+{
+    let ul = document.createElement('ul')
+    var li = document.createElement('li')
+    li.innerHTML = 'Hola amigos punto 1'
+    ul.appendChild(li)
+    // ul.className = 'frenteRojo'
+    console.log(ul)
+    document.body.appendChild(ul)
+}
+
+/*
+Ejercicio: Crear 10 elementos en una lista. y agregarlas al dom
+
+Luego de esto. Crear una funcion que tome como argumento un array
+y agregue en la lista todos los elementos del array
+['item uno', 'item 2', 'item 3']
+*/
