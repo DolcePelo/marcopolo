cacca467499a8c346832bb441bf5041e9fae31ce
andy <arteysoft@gmail.com>
2022-05-04 21:30:51 -0300

    practico 1

---
+++ clase5/js/misscripts.js
@@ -0,0 +1,54 @@
+{
+    /*
+    let miForm = SEL('miFormulario')
+    // validarQueElIdNoSeaVacio('codPais')
+    // validarQueElIdNoSeaVacio('inpNombre')
+
+    function validarQueElIdNoSeaVacio(id) {
+        console.log(SEL(id).value.length)
+        if (SEL(id).value.length === 0) {
+            SEL(id).setCustomValidity('NO HAY NADA!!!')
+        }
+    }
+
+    miForm.addEventListener('submit', e => {
+        e.preventDefault()
+
+        let dato = SEL('inpNombre').value
+        console.log('Submit:' + dato)
+        console.log("Es valido: " + SEL('inpNombre').checkValidity())
+        console.log("Es valido: " + SEL('codPais').checkValidity())
+    })
+
+    SEL('inpNombre').addEventListener('input', e => {
+        // validarQueElIdNoSeaVacio('inpNombre')
+
+
+        if (e.target.value.length === 0) {
+            e.target.setCustomValidity('NADA en 1')
+        }
+
+        console.log(e.target.value)
+    })
+
+
+
+    SEL('codPais').addEventListener('input', e => {
+        console.log(e.target.value)
+
+        window.e = e
+        // validarQueElIdNoSeaVacio('codPais')
+
+        if (e.target.value.length === 0) {
+            e.target.setCustomValidity('NADA en codPais')
+        }
+
+        if (e.target.value.length != 3) {
+            e.target.setCustomValidity('EL LARGO DEL CODIGO DE PAIS DEBE SER 3 DIG')
+        }
+    })
+    */
+
+
+}
+
