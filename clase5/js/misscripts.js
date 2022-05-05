f23f11d756c9c72446b2fd4f205897e146d1c865
andy <arteysoft@gmail.com>
2022-05-04 22:01:47 -0300

    clase #5

--- clase5/js/misscripts.js
+++ clase5/js/misscripts.js
@@ -1,53 +1,40 @@
 {
-    /*
     let miForm = SEL('miFormulario')
-    // validarQueElIdNoSeaVacio('codPais')
-    // validarQueElIdNoSeaVacio('inpNombre')
-
-    function validarQueElIdNoSeaVacio(id) {
-        console.log(SEL(id).value.length)
-        if (SEL(id).value.length === 0) {
-            SEL(id).setCustomValidity('NO HAY NADA!!!')
-        }
-    }
+    let claveValor = {}
+    claveValor['codPais'] = 'chkCodPais'
+    claveValor['inpNombre'] = 'chkNombre'
 
     miForm.addEventListener('submit', e => {
         e.preventDefault()
-
-        let dato = SEL('inpNombre').value
-        console.log('Submit:' + dato)
         console.log("Es valido: " + SEL('inpNombre').checkValidity())
         console.log("Es valido: " + SEL('codPais').checkValidity())
     })
 
     SEL('inpNombre').addEventListener('input', e => {
-        // validarQueElIdNoSeaVacio('inpNombre')
-
-
-        if (e.target.value.length === 0) {
-            e.target.setCustomValidity('NADA en 1')
+        if (e.target.value.length == 0) {
+            SEL('chkNombre').className = 'iconito'
         }
-
-        console.log(e.target.value)
+        else {
+            SEL('chkNombre').className = 'noIconito'
+        }
     })
-
-
 
     SEL('codPais').addEventListener('input', e => {
-        console.log(e.target.value)
-
-        window.e = e
-        // validarQueElIdNoSeaVacio('codPais')
-
-        if (e.target.value.length === 0) {
-            e.target.setCustomValidity('NADA en codPais')
+        if (e.target.value.length == 0) {
+            SEL('chkCodPais').className = 'iconito'
+        }
+        else {
+            SEL('chkCodPais').className = 'noIconito'
         }
 
-        if (e.target.value.length != 3) {
-            e.target.setCustomValidity('EL LARGO DEL CODIGO DE PAIS DEBE SER 3 DIG')
+        if (e.target.value.length == 3) {
+            SEL('chkCodPais').className = 'noIconito'
+        }
+        else {
+            SEL('chkCodPais').className = 'iconito'
+            // e.target.setCustomValidity('EL LARGO DEL CODIGO DE PAIS DEBE SER 3 DIG')
         }
     })
-    */
 
 
 }
