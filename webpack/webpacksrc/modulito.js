9278fcdbd9c05de58e89afb3a709f8bc3de45399
andy <arteysoft@gmail.com>
2022-05-09 21:57:20 -0300

    incluyo webpack

---
+++ webpack/webpacksrc/modulito.js
@@ -0,0 +1,14 @@
+function obtenerOtraCosa() {
+
+}
+
+function obtenerMensaje() {
+    return "hola desde modulito";
+}
+
+function decirHola() {
+    alert(obtenerMensaje())
+}
+
+module.exports = decirHola
+
