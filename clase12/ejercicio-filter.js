26728b2691bd5326f068d1bb70543e72181e3696
andy <arteysoft@gmail.com>
2022-06-06 21:13:29 -0300

    ejercicio map y luego filter

--- clase12/ejercicio-filter.js
+++ clase12/ejercicio-filter.js
@@ -1,3 +1,5 @@
+const _ = require('underscore')
+
 /*
 Crear un objeto que este organizado por tipo de datos
 El tipo crudo de dato ? es el siguiente array
@@ -12,3 +14,30 @@ let arrCrudo = [{}, [], 2, true, {a:1}, 'hola', false, [2,3,5], 'chau']
 // y el value, un array con los valores encontrados de ese tipo
 // de dato
 
+let arrPosicionMasTipo = _.map(arrCrudo, (z, idx) => {
+   return {
+      posicion: idx,
+      tipoDeDato : typeof z,
+      valor: z
+   }
+})
+
+console.log(arrPosicionMasTipo)
+
+/*
+let objResultado = {}
+objResultado.todosLosObjetos = _.filter(arrCrudo, z => typeof z === 'object')
+objResultado.todosLosNumber = _.filter(arrCrudo, z => typeof z === 'number')
+objResultado.todosLosBoolean = _.filter(arrCrudo, z => typeof z === 'boolean')
+objResultado.todosLosString = _.filter(arrCrudo, (z, idx) => {
+   console.log(idx)
+   if (typeof z === 'string') {
+
+   }
+
+})
+
+console.log(arrCrudo)
+console.log(resultado)
+console.log(objResultado)
+*/
